3af8644085bc65a446d702462d47610e65f239ee
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-03-19 03:21:34 -0400

    update all

--- how.js
+++ how.js
@@ -20,3 +20,22 @@ function main() {
 }
 
 
+~
+
+// The snail climbs up 7 feet each day and slips back 2 feet each night. How many days will it take the snail to get out of a well with the given depth?
+
+function main() {
+    var depth = parseInt(readLine(), 10);
+    i = 0;
+    
+    for (; depth > 0;) {
+        i++;
+        
+        depth -= 7
+        if (depth > 0) {
+            depth += 2
+        }
+    }
+    console.log(i);
+}
+    
