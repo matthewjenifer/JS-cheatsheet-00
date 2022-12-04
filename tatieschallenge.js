fc21b8f111893ea659696b229a2a926ba088822d
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-12-04 17:40:09 -0500

    unneeded code removed2 some added

--- tatieschallenge.js
+++ tatieschallenge.js
@@ -1,20 +1,20 @@
 const numArray = [ 2, 5, 9, 12, 208, 333, 18, 11, 0]; //Write a function called evenNumbers that takes an array of numbers as a parameter and returns only an array of even numbers.Do the same for odd numbers.
 
-function evenNumbers() {
-    for(let value of numArray){
-        if(value % 2 == 0) {
-            console.log(value + ' is even')
+function evenNumbers(arr) {
+    for(let val of arr){
+        if(val % 2 == 0) {
+            console.log(val + ' is even')
         }
     }
 };
 
-function oddNumbers() {
-    for(let value of numArray){
-        if(!(value % 2 == 0)) {
-            console.log(value + ' is odd')
+function oddNumbers(arr) {
+    for(let val of arr){
+        if(!(val % 2 == 0)) {
+            console.log(val + ' is odd')
         }
     }
 };
 
-evenNumbers(numArray); // ..i thought this counted 
-oddNumbers(); // ...until I realized I could still call without an argument 
+evenNumbers(numArray);
+oddNumbers();
--- tatieschallenge2.js
+++ tatieschallenge2.js
@@ -2,11 +2,11 @@ const numArray = [ 2, 5, 9, 12, 208, 333, 18, 11, 0] //Create a function that re
 
 function sumOdds(arr) { // the long way
     oddsOnly = []
-    for(var i=0; i<arr.length; i++) {
-        if(arr[i] % 2 !== 0) {oddsOnly.push(arr[i])} //i feel like i can semi sight read arrow functions but i don't know where to begin off top..
-    }
-    sum = oddsOnly.reduce((acc, val) => {return acc + val},0) //..found this syntax on google tho
-    console.log("odds: ", oddsOnly," sum: ", sum)
+        for(var i=0; i<arr.length; i++) {
+            if(arr[i] % 2 !== 0) {oddsOnly.push(arr[i])} //i feel like i can semi sight read arrow functions but i don't know where to begin off top..
+            }
+        sum = oddsOnly.reduce((acc, val) => {return acc + val},0) //..found this syntax on google tho
+    console.log("odds of the given array are: ", oddsOnly," returning a sum of: ", sum)
 }
 
 sumOdds(numArray)
