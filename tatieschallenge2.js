750d875966192048dd1ffb2fee8cc95fd56a0fd7
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-12-05 02:18:33 -0500

    shorthanded

--- tatieschallenge2.js
+++ tatieschallenge2.js
@@ -2,8 +2,9 @@ const numArray = [ 2, 5, 9, 12, 208, 333, 18, 11, 0] //Create a function that re
 
 const sumOdds = (arr) => { 
     oddsOnly = []
-        for(var i=0; i<arr.length; i++){if(arr[i] % 2 !== 0){oddsOnly.push(arr[i])}}
-        sum = oddsOnly.reduce((acc, val) => {return acc + val},0) //reduce refresher: https://youtu.be/3mGbzELRBkM 
+    var i=0
+        for(;i<arr.length;i++){arr[i]%2!==0?oddsOnly.push(arr[i]):0}
+        sum = oddsOnly.reduce((acc,val) => {return acc+val},0) //refresher: https://youtu.be/3mGbzELRBkM 
     console.log("odds of the given array are: ", oddsOnly," returning a sum of: ", sum)
 }
 
@@ -19,3 +20,13 @@ function bob(a) {
 //..is the same as
 
 const bob2 = (a) => a + 100;
+
+function main() {
+    var hour = parseInt(readLine(), 10);
+    // Your code goes here
+
+    time = hour >= 0 && hour <= 12 ? "am" : "pm";
+
+    console.log(time)
+}
+
