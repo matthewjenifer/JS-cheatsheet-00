4dd5a2f3f34df1b4e1ef57035fff41d05baa4876
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-12-16 23:15:17 -0500

    notes on how exactly? 2

--- debtcalc.js
+++ debtcalc.js
@@ -1,12 +1,9 @@
 function calculateTimeToPayOffDebt(debt, interestRate, monthlyIncome) {
-    // Calculate the monthly interest rate by dividing the annual interest rate by 12
-    var monthlyInterestRate = interestRate / 12;
+    var monthlyInterestRate = interestRate / 12; // Calculate the monthly interest rate by dividing the annual interest rate by 12
     
-    // Calculate the total number of payments needed to pay off the debt by dividing the debt by the monthly income and then multiplying by the monthly interest rate
-    var totalPayments = debt / monthlyIncome * monthlyInterestRate;
+    var totalPayments = debt / monthlyIncome * monthlyInterestRate; // Calculate the total number of payments needed to pay off the debt by dividing the debt by the monthly income and then multiplying by the monthly interest rate
     
-    // Print the number of payments to the screen
-    console.log("It will take a total of " + totalPayments + " payments to pay off the debt.");
+    console.log("It will take a total of " + totalPayments + " payments to pay off the debt."); // Print the number of payments to the screen
 }
 
-calculateTimeToPayOffDebt(20279, 17, 50);
+calculateTimeToPayOffDebt(20279, 17, 2000);
